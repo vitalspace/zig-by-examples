f82dcd8cf6e8edae937e8b7c9929cd09500b9bc8
vitalspace <myglobalsounds@gmail.com>
2023-08-13 17:27:33 -0600

    fixed

--- fs.zig
+++ fs.zig
@@ -3,6 +3,7 @@ const print = std.debug.print;
 
 pub fn main() !void {
     try createFile("./example.txt", "Hello world");
+    try openFile("./example.txt");
 }
 
 pub fn createFile(fileName: []const u8, content: []const u8) !void {
@@ -13,9 +14,6 @@ pub fn createFile(fileName: []const u8, content: []const u8) !void {
 }
 
 pub fn openFile(fileName: []const u8) !void {
-    const content = try std.fs.cwd().readFileAlloc(
-        std.heap.page_allocator, 
-        fileName, 
-        std.math.maxInt(usize));
+    const content = try std.fs.cwd().readFileAlloc(std.heap.page_allocator, fileName, std.math.maxInt(usize));
     print("{s}", .{content});
 }
