3c09225898a33bb2a242ee12028f4bfba710d560
vitalspace <myglobalsounds@gmail.com>
2023-10-16 00:05:48 -0600

    added array string

--- arrays.zig
+++ arrays.zig
@@ -3,11 +3,18 @@ const print = std.debug.print;
 
 pub fn main() !void {
     const numbers = [_]i32{ 1, 2, 3, 4, 5, 6 };
+    const names = [_][]const u8{"lucas", "sam", "vital", "victoria"};
 
-    const long = std.mem.len(numbers);
-    print("The leng is: {d}\n", .{long});
+    // const long = std.mem.len(numbers);
+    // print("The leng is: {d}\n", .{long});
 
     for (numbers) |number| {
         print("{d}\n", .{number});
+    }
+
+    print("{s}\n", .{""});
+
+    for (names) |name| {
+        print("Hello, my name is{s}\n", .{name});
     }
 }
